db46638055c92fa1ba206f440418a36096fa73c9
alumno <alumno@10.0.2.15>
2017-04-04 13:31:06 +0200

    Subir archivos a GitHub

---
+++ Proyecto1/src/proyecto1/Persona.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyecto1;
+
+/**
+ *
+ * @author alumno
+ */
+public class Persona {
+    
+    private String numeroDeTelefono;
+    
+    public Persona (String numeroDeTelefono){
+    
+    super();
+    this.numeroDeTelefono = numeroDeTelefono;
+   
+    }
+
+    public String getNumeroDeTelefono() {
+        return numeroDeTelefono;
+    }
+
+    public void setNumeroDeTelefono(String numeroDeTelefono) {
+        this.numeroDeTelefono = numeroDeTelefono;
+    }
+    
+    
+    
+    
+}
